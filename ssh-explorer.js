55ee88964a7581db2fc42e180a97d97d3bd86eb9
Shubham Sangle <shubham.sangle@lodhagroup.com>
2025-03-24 15:06:05 +0530

    FEAT : commander CLI intefrated.

--- ssh-explorer.js
+++ ssh-explorer.js
@@ -1,5 +1,7 @@
 'use strict'
 
+import { Command } from 'commander';
+
 import SSHConnect from './connect-ssh.js'
 import helper from './helper.js'
 
@@ -9,20 +11,48 @@ import helper from './helper.js'
  * 1. Using SSH Key (Default) -> privateKey
  * 2. Using Password -> password
  * 
-    Config options:
-    host           - SSH server hostname or IP address
-    port           - SSH port (default: 22)
-    username       - SSH login username (Default : root)
-    password       - Password for authentication (if used)
-    authMethod     - 'password' or 'privateKey'(default)
-    privateKeyPath - File path to private key (for key-based auth)
- */
+Config options:
+host           - SSH server hostname or IP address
+port           - SSH port (default: 22)
+username       - SSH login username (Default : root)
+password       - Password for authentication (if used)
+authMethod     - 'password' or 'privateKey'(default)
+privateKeyPath - File path to private key (for key-based auth)
+*/
+
+/**
+ * Get options from the terminal
+*/
+const program = new Command();
+
+program.name('ssh-file-explorer')
+    .usage('[options] <host>')
+    .description('A lightweight CLI tool for establishing SSH connections using password or private key authentication and exploring the files present in server. Easily connect to remote servers via terminal with flexible config options.')
+    .version('1.0.0');
+
+program
+    .option('-p, --port <number>', 'SSH port', 22)
+    .option('-u, --username <username>', 'SSH login username', 'root')
+    .option('-w, --password <password>', 'Password for authentication')
+    .option('--authType <type>', 'Set authentication type ("password" or "privateKey")', 'privateKey')
+    .option('--privateKeyPath <path>', 'Path to private key file')
+    .argument('<host>', 'SSH server hostname or IP address')
+
+program.parse(process.argv);
+const {
+    port, username, password,
+    authType, privateKeyPath
+} = program.opts() ?? {}
+
+const [host] = program.args ?? []
 
 const sshConnection = await new SSHConnect({
-    host: "",
+    host, port,
+    username, password,
+    authMethod : authType, privateKeyPath
 })
 
-if(!sshConnection.isConnected()) {
+if (!sshConnection.isConnected()) {
     console.error(helper.errorMessages.login_failed)
     process.exit(1)
 }
